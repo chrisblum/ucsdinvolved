ea09020db6214a48db3a6986dfe932894517c3e9
Chris Blum <chris.blum2@gmail.com>
2015-02-03 21:00:35 +0000

    final steps still missing new friend

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,11 @@
 var data = require("../data.json");
 
 exports.addFriend = function(req, res) {    
-	// Your code goes here
+	console.log(data);
+	res.render('add', data);
+//	var name = res.query.name;
+//	var description = res.query.description;
+//	console.log(name);
+//	var newFriend = ',{"name": ' + name + ',"description": ' + description + ',"imageURL": "http://lorempixel.com/400/400/people"}';
+//	data["friends"].push(newFriend);
  }
