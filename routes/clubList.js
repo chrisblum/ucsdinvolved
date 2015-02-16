d5be9aff0f7b63550a2c97e40c8153ea6bbbb61a
Chris Blum <cblum@ucsd.edu>
2015-02-16 12:03:57 -0800

    as

---
+++ routes/clubList.js
@@ -0,0 +1,11 @@
+var data = require("../data.json");
+
+exports.addFriend = function(req, res) {    
+	console.log(data);
+	res.render('list', data);
+//	var name = res.query.name;
+//	var description = res.query.description;
+//	console.log(name);
+//	var newFriend = ',{"name": ' + name + ',"description": ' + description + ',"imageURL": "http://lorempixel.com/400/400/people"}';
+//	data["friends"].push(newFriend);
+ }
