c6866ce04b0ea3212f3c0fe038859d7e5106c2d3
Chris Blum <cblum@ucsd.edu>
2015-02-19 10:11:06 -0800

    login page

--- public/js/clubtriton.js
+++ public/js/clubtriton.js
@@ -3,6 +3,7 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
+	var from,to,subject,email,text;
 })
 
 /*
@@ -13,8 +14,30 @@ function initializePage() {
 
 	$(".nameselection").click(anagramthing);
 	$(".testclass").click(getData);
+	// $("#submit").click(sendMessage);
 }
 
+// function sendMessage() {
+// 		to="crexmob@gmail.com";
+//         subject=$("#namer").val();
+//         email=$("#email").val();
+//         text=$("#message").val();
+//         $("#inprogress").text("Sending E-mail...Please wait");
+//         console.log("Sendingggggggg");
+//         $.get("http://localhost:5000/send",{to:to,subject:subject,email:email,text:text},function(data){
+//         if(data=="sent")
+//         {
+//             $("inprogress").empty().html("
+
+// Email has been sent at "+to+" . Please check inbox !
+
+// ");
+//         }
+
+// });
+//     });
+
+}
 function getData(e) {
 	$.get("../../data.json", callbackFn);
 	console.log("working");
