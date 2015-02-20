d9f93d47d20265c6cebc07ed598086c03df58036
Chris Blum <cblum@ucsd.edu>
2015-02-20 00:36:59 -0800

    added search

--- routes/search.js
+++ routes/search.js
@@ -3,10 +3,36 @@ var data = require("../data.json");
 exports.searching = function(req, res) {   
 	searchvalue = req.body.searchinput; 
 	console.log(searchvalue);
-	var new_arr = {};
+// 	var new_arr = filter(data.friends, {
+// 		link: function(searchvalue) {
+// 			return link == searchvalue;}});
+
+// 	function filter( array, filters ) {
+
+//     var ret = [],
+//         i = 0, l = array.length,
+//         filter;
+
+//     all: for ( ; i < l; ++i ) {
+
+//         for ( filter in filters ) {
+//             if ( !filters[filter](array[i][filter]) ) {
+//                 continue all;
+//             }
+//         }
+
+//         ret[ret.length] = array[i];
+
+//     }
+
+//     return ret;
+
+// }
+
+	
 	// new_arr = $.grep(data, function(n,i){
 	//  	return(n.name == 'searchvalue');
 	//  })
-	res.render('club', searchvalue);
+	res.render('search', { 'searchvalue' : searchvalue} );
 
  }
