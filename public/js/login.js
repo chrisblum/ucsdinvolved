081546a7a06ea36ef19b9013268d5789670600f5
Chris Blum <cblum@ucsd.edu>
2015-03-04 20:38:34 -0800

    adasd

--- public/js/login.js
+++ public/js/login.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 	});
 
 
-		$("body").on("click","#closeInfoButton",function(){
+		$("body").on("click",".closeInfoButton",function(){
 		$("#infoparagraph").slideUp("fast");
 		$("#showInfoButton").slideDown("fast");
 
