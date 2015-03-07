2e8717385956ceb64b5f09da1deab4a20fff2b07
Chris Blum <cblum@ucsd.edu>
2015-03-06 21:31:48 -0800

    dasas

--- routes/home.js
+++ routes/home.js
@@ -3,12 +3,14 @@ var data = require("../data.json");
 exports.gridsystem = function(req, res) {    
 	console.log(data);
 
-	var random_num = Math.random();
+	// var random_num = Math.random();
 
-  if(random_num > 0.5){
-    res.render("home", data);
-  }else{
-    res.render("homealternate", data)
-  }
+  // if(random_num > 0.5){
+  //   res.render("home", data);
+  // }else{
+  //   res.render("homealternate", data)
+  // }
+
+  res.render("home", data);
 
  }
